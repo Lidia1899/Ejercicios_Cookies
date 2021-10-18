73cad20545583aae564d0431bc06c2b210c6095f
Lidia1899 <lhlidiaherrero1999@gmail.com>
2021-10-18 13:01:29 +0200

    COMMIT

--- cookies_3.php
+++ cookies_3.php
@@ -1,5 +1,18 @@
 <?php 
 
+
+if(isset($_REQUEST['entrar'])){ //Si le he pulsado
+    if(isset($_REQUEST["guardar_clave"])){
+ 
+    
+    $nombre=htmlspecialchars($_REQUEST['usuario']); //Los datos recogidos se convierten en cadena, por lo que es inofensivo
+    $clave=$_REQUEST['clave'];
+    setcookie("Usuario", $nombre, time()+ 3600);
+    setcookie("clave", $clave, time()+ 3600);
+    print_r ($_COOKIE);
+    echo $_COOKIE['usuario'], '<br>', $_COOKIE['clave']; 
+    }
+}
 include 'head.php';
 echo'<h1>Autenticar usuario y guardar en una cookie con PHP</h1><br><br>';
 echo'<form action="prueba-cookies.php" method="post">
@@ -18,7 +31,5 @@ echo'<form action="prueba-cookies.php" method="post">
 </tr>
 </table>
 </form> ';
-
-
-
 include 'pie.php';
+?>
